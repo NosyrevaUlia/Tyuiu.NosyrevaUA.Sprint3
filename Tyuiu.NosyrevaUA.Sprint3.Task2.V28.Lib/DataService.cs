d1ab533768cdfe040ceba017cec3c42b2b5dfac9
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-10-19 15:43:07 +0500

    Task2

---
+++ Tyuiu.NosyrevaUA.Sprint3.Task2.V28.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.NosyrevaUA.Sprint3.Task2.V28.Lib
+{
+    public class DataService : ISprint3Task2V28
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double mult = 1;
+            do
+            {
+                mult = mult * ((Math.Pow(2, startValue) / (startValue + 1)) * Math.Cos(1.8));
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(mult, 3);
+        }
+    }
+}
---
+++ Tyuiu.NosyrevaUA.Sprint3.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosyrevaUA.Sprint3.Task2.V28.Lib;
+
+namespace Tyuiu.NosyrevaUA.Sprint3.Task2.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetMultiplySeries(1, 9);
+            Assert.AreEqual(res, -15.642);
+        }
+    }
+}
