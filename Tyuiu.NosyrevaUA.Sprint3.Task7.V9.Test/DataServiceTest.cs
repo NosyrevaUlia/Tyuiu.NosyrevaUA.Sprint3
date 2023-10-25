5f82f02e97667b08c296402b574da68732a8ab6a
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-10-25 23:33:08 +0500

    Task7

---
+++ Tyuiu.NosyrevaUA.Sprint3.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosyrevaUA.Sprint3.Task7.V9.Lib;
+
+namespace Tyuiu.NosyrevaUA.Sprint3.Task7.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int start = -5;
+            int stop = 5;
+            int len = stop - start + 1;
+
+            double[] valwait;
+            valwait = new double[len];
+
+            valwait[0] = -61.79;
+            valwait[1] = 4.07;
+            valwait[2] = 0.23;
+            valwait[3] = 11.73;
+            valwait[4] = -11.41;
+            valwait[5] = -3.0;
+            valwait[6] = 0.31;
+            valwait[7] = 2.69;
+            valwait[8] = 5.83;
+            valwait[9] = 5.11;
+            valwait[10] = 40.64;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(start, stop);
+            CollectionAssert.AreEqual(valwait, res);
+        }
+    }
+}
---
+++ Tyuiu.NosyrevaUA.Sprint3.Task7.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NosyrevaUA.Sprint3.Task7.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NosyrevaUA.Sprint3.Task7.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2e286d5-2def-4f44-84c4-4a1a9acec237")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
